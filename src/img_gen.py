d6d8e5ca8b8a5a059367af822179e00e619b9ef2
Andre <andrefranceschini@gmail.combitcoind>
2025-01-25 15:26:33 +0100

    “Update”

--- src/img_gen.py
+++ src/img_gen.py
@@ -6,6 +6,10 @@ import ipywidgets as widgets
 from config.config_colab import api_token
 from colab import output
 
+
+# Step 11: Create an Output Widget to Display Results
+output = widgets.Output()
+
 # Step 13: Define the Image Generation Function
 def generate_image(button):
     # Clear the output widget (only clears the image and logs, not the widgets)
